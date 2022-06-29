4e5a31f9ae2d829c02639faed677a63f4fc7e6fc
Milena Madeira <milenamuchenski@gmail.com>
2022-06-29 10:58:23 -0300

    title

--- View/lib/Title.cs
+++ View/lib/Title.cs
@@ -6,7 +6,6 @@ namespace Views.Lib
 {
     public class Title : Label
     {
-
         public Title(string Titulo, SizeScreen W)
         {
 
@@ -17,17 +16,19 @@ namespace Views.Lib
                     this.Size = new Size(900, 40);
                     break;
                 case SizeScreen.Medium:
-                    this.Size = new Size(500, 40);
+                    this.Size = new Size(600, 40);
+                    break;
+                case SizeScreen.Long:
+                    this.Size = new Size(300, 40);
                     break;
                 default:
                     this.Size = new Size(300, 40);
-                    
                     break;
             }
             this.BackColor = Color.PaleTurquoise;
             this.Location = new Point(0, 0);
             this.Font = new Font(FontFamily.GenericSansSerif, 12.0F, FontStyle.Bold);
-
+            
         }       
     }
      
