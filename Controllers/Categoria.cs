2d0b69337eaf424e525751fe18fab407a3f600a3
Milena Madeira <milenamuchenski@gmail.com>
2022-06-29 21:01:26 -0300

    categoria controller

--- Controllers/Categoria.cs
+++ Controllers/Categoria.cs
@@ -42,7 +42,7 @@ namespace Controllers
             {
                 throw new Exception("Categoria não encontrada.");
             }
-            
+
             Models.Categoria.AlterarCategoria(Id, Nome, Descricao);
             return categoria;
         }
@@ -54,7 +54,7 @@ namespace Controllers
             Categoria categoria = Categoria.GetCategoria(Id);
             Categoria.RemoverCategoria(categoria);
 
-             if (categoria == null)
+            if (categoria == null)
             {
                 throw new Exception("Categoria não encontrada.");
             }
@@ -62,16 +62,16 @@ namespace Controllers
             return categoria;
         }
 
-         public static Categoria GetCategoria(
-            int Id
-        )
+        public static Categoria GetCategoria(
+           int Id
+       )
         {
             Categoria categoria = (
                 from Categoria in Categoria.GetCategorias()
-                    where Categoria.Id == Id
-                    select Categoria
+                where Categoria.Id == Id
+                select Categoria
             ).First();
-            
+
             if (categoria == null)
             {
                 throw new Exception("Categoria não encontrada");
